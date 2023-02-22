5d580c18b9efacf11fdcf25076edd91a0e93eae5
Marcos Saldivia D <marcos.saldivia@gmail.com>
2023-02-22 11:15:17 -0300

    Added seeder script to populate the database

--- src/seeders/20230215164106-demo-user.js
+++ src/seeders/20230215164106-demo-user.js
@@ -12,6 +12,34 @@ module.exports = {
 		 *   isBetaMember: false
 		 * }], {});
 		 */
+		await queryInterface.bulkInsert(  
+			'Users',
+			[
+				{
+				id : '69a6d512-03fa-4f73-854d-56b6aab14c45',
+				name: 'Demo',
+				surname: 'Demo',
+				username: 'demo_d',
+				email: 'demo@gmail.com',
+				password: '$2b$10$eWBn1ykJ7sD4SJytJm5Jd.WxCoHT60ZdcQ1UtIK4Q.2shBiFuWr0m',
+				confirmed: true,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				},
+				{
+				id : '4bd69f8-350c-44db-a435-a41dcb90c9b3',
+				name: 'User',
+				surname: 'User',
+				username: 'user_d',
+				email: 'user@gmail.com',
+				password: '$2b$10$LfpYHJBrnOwQ2Lku3MVixO.eagVEio0OnDfs7C9I3SDdS6JNhYWLW',
+				confirmed: true,				
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				},
+			],
+			{},
+    );
 	},
 
 	async down(queryInterface, Sequelize) {
@@ -21,5 +49,6 @@ module.exports = {
 		 * Example:
 		 * await queryInterface.bulkDelete('People', null, {});
 		 */
+		await queryInterface.bulkDelete('Users', null, {});
 	},
 }
